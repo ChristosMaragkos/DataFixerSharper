cd2f096bb5ec2c73260d3d23e1ec89618aa252b4
Christos Maragkos <whitetowersoftware@gmail.com>
2026-02-13 23:19:44 +0200

    Add nongeneric IDynamicOps wrapper

--- WhiteTowerGames.DataFixerSharper/Abstractions/IDynamicOps.cs
+++ WhiteTowerGames.DataFixerSharper/Abstractions/IDynamicOps.cs
@@ -3,15 +3,36 @@ using System.ComponentModel;
 namespace WhiteTowerGames.DataFixerSharper.Abstractions;
 
 [EditorBrowsable(EditorBrowsableState.Never)]
-public interface IDynamicOps;
+public interface IDynamicOps
+{
+    object Empty();
+    object CreateNumeric(decimal number);
+    object CreateString(string value);
+    object CreateBool(bool value);
+
+    DataResult<decimal> GetNumber(object input);
+    DataResult<string> GetString(object input);
+    DataResult<bool> GetBool(object input);
+
+    object CreateList(IEnumerable<object> elements);
+    DataResult<IEnumerable<object>> ReadAsStream(object input);
+
+    object CreateMap(IEnumerable<KeyValuePair<object, object>> map);
+    DataResult<IEnumerable<KeyValuePair<object, object>>> ReadAsMap(object input);
+    object Merge(object key, object value);
+    object MergeAndAppend(object map, object key, object value);
+
+    object AppendToPrefix(object prefix, object value);
+    object RemoveFromInput(object input, object value);
+}
 
 public interface IDynamicOps<T> : IDynamicOps
 {
     #region Value Creation
-    T Empty();
-    T CreateNumeric(decimal number);
-    T CreateString(string value);
-    T CreateBool(bool value);
+    new T Empty();
+    new T CreateNumeric(decimal number);
+    new T CreateString(string value);
+    new T CreateBool(bool value);
     #endregion
 
     #region Value Reading
@@ -42,29 +63,67 @@ public interface IDynamicOps<T> : IDynamicOps
     T AppendToPrefix(T prefix, T value);
     T RemoveFromInput(T input, T value);
     #endregion
+
+    #region Default Implementations
+    object IDynamicOps.Empty() => Empty()!;
+    object IDynamicOps.CreateNumeric(decimal number) => CreateNumeric(number)!;
+    object IDynamicOps.CreateString(string value) => CreateString(value)!;
+    object IDynamicOps.CreateBool(bool value) => CreateBool(value)!;
+
+    DataResult<decimal> IDynamicOps.GetNumber(object input) => GetNumber((T)input);
+    DataResult<string> IDynamicOps.GetString(object input) => GetString((T)input);
+    DataResult<bool> IDynamicOps.GetBool(object input) => GetBool((T)input);
+
+    object IDynamicOps.CreateList(IEnumerable<object> elements) =>
+        CreateList((IEnumerable<T>)elements)!;
+
+    DataResult<IEnumerable<object>> IDynamicOps.ReadAsStream(object input) =>
+        ReadAsStream((T)input).Map(stream => stream.Select(item => (object)item!));
+
+    object IDynamicOps.CreateMap(IEnumerable<KeyValuePair<object, object>> map) =>
+        CreateMap((IEnumerable<KeyValuePair<T, T>>)map)!;
+
+    DataResult<IEnumerable<KeyValuePair<object, object>>> IDynamicOps.ReadAsMap(object input) =>
+        ReadAsMap((T)input)
+            .Map(map =>
+                map.Select(kvp => new KeyValuePair<object, object>(
+                    (object)kvp.Key!,
+                    (object)kvp.Value!
+                ))
+            );
+
+    object IDynamicOps.Merge(object key, object value) => Merge((T)key, (T)value)!;
+    object IDynamicOps.MergeAndAppend(object map, object key, object value) =>
+        MergeAndAppend((T)map, (T)key, (T)value)!;
+
+    object IDynamicOps.AppendToPrefix(object prefix, object value) =>
+        AppendToPrefix((T)prefix, (T)value)!;
+    object IDynamicOps.RemoveFromInput(object input, object value) =>
+        RemoveFromInput((T)input, (T)value)!;
+    #endregion
 }
 
 public static class DynamicOpsExtensions
 {
-    public static T CreateInt32<T>(this IDynamicOps<T> ops, int value) => ops.CreateNumeric(value);
+    public static object CreateInt32(this IDynamicOps ops, int value) => ops.CreateNumeric(value);
 
-    public static T CreateInt64<T>(this IDynamicOps<T> ops, long value) => ops.CreateNumeric(value);
+    public static object CreateInt64(this IDynamicOps ops, long value) => ops.CreateNumeric(value);
 
-    public static T CreateFloat<T>(this IDynamicOps<T> ops, float value) =>
+    public static object CreateFloat(this IDynamicOps ops, float value) =>
         ops.CreateNumeric((decimal)value);
 
-    public static T CreateDouble<T>(this IDynamicOps<T> ops, double value) =>
+    public static object CreateDouble(this IDynamicOps ops, double value) =>
         ops.CreateNumeric((decimal)value);
 
-    public static DataResult<int> GetInt32<T>(this IDynamicOps<T> ops, T input) =>
+    public static DataResult<int> GetInt32(this IDynamicOps ops, object input) =>
         ops.GetNumber(input).Map(d => (int)d);
 
-    public static DataResult<long> GetInt64<T>(this IDynamicOps<T> ops, T input) =>
+    public static DataResult<long> GetInt64(this IDynamicOps ops, object input) =>
         ops.GetNumber(input).Map(d => (long)d);
 
-    public static DataResult<float> GetFloat<T>(this IDynamicOps<T> ops, T input) =>
+    public static DataResult<float> GetFloat(this IDynamicOps ops, object input) =>
         ops.GetNumber(input).Map(d => (float)d);
 
-    public static DataResult<double> GetDouble<T>(this IDynamicOps<T> ops, T input) =>
+    public static DataResult<double> GetDouble(this IDynamicOps ops, object input) =>
         ops.GetNumber(input).Map(d => (double)d);
 }
