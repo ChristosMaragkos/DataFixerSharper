5a751a89cc9e8779663030dbfa1f35efd48052e9
Christos Maragkos <whitetowersoftware@gmail.com>
2026-02-13 23:42:18 +0200

    Add Dictionary codec test

--- WhiteTowerGames.DataFixerSharper.Tests/Collections/CollectionCodecs.cs
+++ WhiteTowerGames.DataFixerSharper.Tests/Collections/CollectionCodecs.cs
@@ -1,3 +1,5 @@
+using WhiteTowerGames.DataFixerSharper.Codecs;
+
 namespace WhiteTowerGames.DataFixerSharper.Tests.Collections;
 
 public class CollectionCodecs
@@ -18,5 +20,23 @@ public class CollectionCodecs
 
         // Then
         Assert.True(numbers.SequenceEqual(decoded));
+    }
+
+    [Fact]
+    public void DictionaryCodec_Roundtrip_Works()
+    {
+        // Given
+        var dict = new Dictionary<string, int>() { { "zero", 0 }, { "one", 1 } };
+        var codec = Codec.Dictionary<string, int>(BuiltinCodecs.String, BuiltinCodecs.Int32);
+
+        // When
+        var encoded = codec.EncodeStart(JsonOps, dict);
+        var decoded = codec.Parse(JsonOps, encoded.GetOrThrow());
+        var value = decoded.GetOrThrow();
+
+        // Then
+        Assert.False(encoded.IsError);
+        Assert.False(decoded.IsError);
+        Assert.True(dict.SequenceEqual(value));
     }
 }
