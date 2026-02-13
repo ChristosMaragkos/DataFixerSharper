6df082fde60bf09fdf37c751df8b5417fb6582ca
Christos Maragkos <whitetowersoftware@gmail.com>
2026-02-13 11:44:14 +0200

    Add enum codecs

--- WhiteTowerGames.DataFixerSharper/BuiltinCodecs.cs
+++ WhiteTowerGames.DataFixerSharper/BuiltinCodecs.cs
@@ -53,5 +53,88 @@ public static class BuiltinCodecs
             )
     );
 
-    // TODO: Enums (by value, by name, flags to string array)
+    public static Codec<TEnum> EnumByValue<TEnum>()
+        where TEnum : struct, Enum
+    {
+        return Int32.Unsafe2SafeMap(
+            value => (int)(object)value,
+            integer =>
+                Enum.IsDefined(typeof(TEnum), integer)
+                    ? DataResult<TEnum>.Success((TEnum)(object)integer)
+                    : DataResult<TEnum>.Fail(
+                        $"Parsed value {integer} was not a valid value for enum {typeof(TEnum).FullName}"
+                    )
+        );
+    }
+
+    public static Codec<TEnum> EnumByName<TEnum>()
+        where TEnum : struct, Enum
+    {
+        return String.Unsafe2SafeMap(
+            value => Enum.Format(typeof(TEnum), value, "F"),
+            str =>
+                Enum.TryParse(typeof(TEnum), str, false, out object? value)
+                    ? DataResult<TEnum>.Success((TEnum)value)
+                    : DataResult<TEnum>.Fail(
+                        $"Parsed string value '{str}' does not match any members of enum {typeof(TEnum).FullName}"
+                    )
+        );
+    }
+
+    public static Codec<TEnum> FlagsByName<TEnum>()
+        where TEnum : struct, Enum
+    {
+        return String
+            .ForArray()
+            .Unsafe2SafeMap<TEnum>(
+                value => FlagsToStringArray(value),
+                strArray => StringArrayToFlags<TEnum>(strArray)
+            );
+    }
+
+    private static string[] FlagsToStringArray<TEnum>(TEnum value)
+        where TEnum : struct, Enum
+    {
+        var result = new List<string>();
+        var underlying = Convert.ToUInt64(value);
+
+        foreach (var enumValue in Enum.GetValues<TEnum>())
+        {
+            var bits = Convert.ToUInt64(enumValue);
+
+            if (bits == 0 || (bits & (bits - 1)) != 0) // Evil bit hack
+                continue;
+
+            if ((underlying & bits) == bits)
+            {
+                result.Add(enumValue.ToString());
+            }
+        }
+
+        return result.ToArray();
+    }
+
+    private static DataResult<TEnum> StringArrayToFlags<TEnum>(string[] strArray)
+        where TEnum : struct, Enum
+    {
+        if (!typeof(TEnum).IsDefined(typeof(FlagsAttribute), false))
+            return DataResult<TEnum>.Fail(
+                $"{typeof(TEnum).FullName} must be an enum annotated with [Flags]"
+            );
+
+        ulong flags = 0;
+        foreach (var str in strArray)
+        {
+            if (!Enum.TryParse(typeof(TEnum), str, false, out var value))
+                return DataResult<TEnum>.Fail(
+                    $"Parsed string value '{str}' does not match and members of enum {typeof(TEnum).FullName}",
+                    (TEnum)(object)flags
+                );
+
+            var enumValue = Convert.ToUInt64(value);
+            flags |= enumValue;
+        }
+
+        return DataResult<TEnum>.Success((TEnum)(object)flags);
+    }
 }
