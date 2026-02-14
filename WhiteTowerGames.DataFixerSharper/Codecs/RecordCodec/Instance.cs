d553f13269ab41d7af7a928d196c3b1be9a84ce8
Christos Maragkos <whitetowersoftware@gmail.com>
2026-02-14 09:49:47 +0200

    Add 5-arity RecordCodecBuilder implementation

--- WhiteTowerGames.DataFixerSharper/Codecs/RecordCodec/Instance.cs
+++ WhiteTowerGames.DataFixerSharper/Codecs/RecordCodec/Instance.cs
@@ -6,4 +6,25 @@ public class Instance<T>
 
     public Fm2<T, TF, TF1> WithFields<TF, TF1>(IFieldCodec<T, TF> f1, IFieldCodec<T, TF1> f2) =>
         new Fm2<T, TF, TF1>(f1, f2);
+
+    public Fm3<T, TF, TF1, TF2> WithFields<TF, TF1, TF2>(
+        IFieldCodec<T, TF> f1,
+        IFieldCodec<T, TF1> f2,
+        IFieldCodec<T, TF2> f3
+    ) => new Fm3<T, TF, TF1, TF2>(f1, f2, f3);
+
+    public Fm4<T, TF, TF1, TF2, TF3> WithFields<TF, TF1, TF2, TF3>(
+        IFieldCodec<T, TF> f1,
+        IFieldCodec<T, TF1> f2,
+        IFieldCodec<T, TF2> f3,
+        IFieldCodec<T, TF3> f4
+    ) => new Fm4<T, TF, TF1, TF2, TF3>(f1, f2, f3, f4);
+
+    public Fm5<T, TF, TF1, TF2, TF3, TF4> WithFields<TF, TF1, TF2, TF3, TF4>(
+        IFieldCodec<T, TF> f1,
+        IFieldCodec<T, TF1> f2,
+        IFieldCodec<T, TF2> f3,
+        IFieldCodec<T, TF3> f4,
+        IFieldCodec<T, TF4> f5
+    ) => new Fm5<T, TF, TF1, TF2, TF3, TF4>(f1, f2, f3, f4, f5);
 }
