813842b7ee0343dcb731cd213086242322ba05e5
Christos Maragkos <whitetowersoftware@gmail.com>
2026-02-14 03:32:30 +0200

    Improve RecordCodec tests

--- WhiteTowerGames.DataFixerSharper.Tests/Composition/CodecBuilding.cs
+++ WhiteTowerGames.DataFixerSharper.Tests/Composition/CodecBuilding.cs
@@ -9,6 +9,10 @@ public class CodecBuilding
 
     public sealed record Person(string Name, int Age = 0);
 
+    public sealed record Relationship(Person Person1, Person Person2);
+
+    public sealed record FriendGroup(params Person[] Members);
+
     public static IEnumerable<object[]> People()
     {
         yield return new object[] { new Person("John Doe", 18) };
@@ -25,6 +29,25 @@ public class CodecBuilding
                 .WithCtor((name, age) => new Person(name, age))
     );
 
+    private static readonly Codec<Relationship> RelationshipCodec =
+        RecordCodecBuilder.Create<Relationship>(instance =>
+            instance
+                .WithFields(
+                    PersonCodec.Field((Relationship rel) => rel.Person1, "Person_1"),
+                    PersonCodec.Field((Relationship rel) => rel.Person2, "Person_2")
+                )
+                .WithCtor((p1, p2) => new Relationship(p1, p2))
+        );
+
+    private static readonly Codec<FriendGroup> FriendGroupCodec =
+        RecordCodecBuilder.Create<FriendGroup>(instance =>
+            instance
+                .WithFields(
+                    PersonCodec.ForArray().Field((FriendGroup group) => group.Members, "members")
+                )
+                .WithCtor(people => new FriendGroup(people))
+        );
+
     [Theory]
     [MemberData(nameof(People))]
     public void RecordCodec_RoundTrip_ReturnsSame(Person person)
@@ -37,5 +60,58 @@ public class CodecBuilding
         Assert.False(encoded.IsError, encoded.ErrorMessage);
         Assert.False(decoded.IsError, decoded.ErrorMessage);
         Assert.Equal(person, decoded.GetOrThrow());
+    }
+
+    [Fact]
+    public void RecordCodec_Deterministic_ForCollection()
+    {
+        // Given
+        var people = new Person[] { new Person("John Doe", 18), new Person("Jane Doe", 21) };
+        var peopleCodec = PersonCodec.ForArray();
+
+        // When
+        var encoded = peopleCodec.EncodeStart(JsonOps, people);
+        var decoded = peopleCodec.Parse(JsonOps, encoded.GetOrThrow());
+
+        // Then
+        Assert.False(encoded.IsError, encoded.ErrorMessage);
+        Assert.False(decoded.IsError, decoded.ErrorMessage);
+        Assert.True(decoded.GetOrThrow().SequenceEqual(people));
+    }
+
+    [Theory]
+    [MemberData(nameof(People))]
+    public void RecordCodec_Works_WhenNested(Person person)
+    {
+        // Given
+        var rel = new Relationship(person, person);
+
+        // When
+        var encoded = RelationshipCodec.EncodeStart(JsonOps, rel);
+        var decoded = RelationshipCodec.Parse(JsonOps, encoded.GetOrThrow());
+
+        // Then
+        Assert.False(encoded.IsError, encoded.ErrorMessage);
+        Assert.False(decoded.IsError, decoded.ErrorMessage);
+        Assert.Equal(rel, decoded.GetOrThrow());
+    }
+
+    [Theory]
+    [MemberData(nameof(People))]
+    public void RecordCodec_Works_WithNestedEnumerable(Person person)
+    {
+        // Given
+        var group = new FriendGroup(person, person, person, person);
+
+        // When
+        var encoded = FriendGroupCodec.EncodeStart(JsonOps, group);
+        var decoded = FriendGroupCodec.Parse(JsonOps, encoded.GetOrThrow());
+
+        Console.WriteLine(encoded.GetOrThrow().ToJsonString());
+
+        // Then
+        Assert.False(encoded.IsError, encoded.ErrorMessage);
+        Assert.False(decoded.IsError, decoded.ErrorMessage);
+        Assert.Equal(group.Members, decoded.GetOrThrow().Members);
     }
 }
