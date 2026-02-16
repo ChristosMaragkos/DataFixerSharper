906bd84a7541c9e301ea95327b687efb4eb9fcff
Christos Maragkos <whitetowersoftware@gmail.com>
2026-02-16 05:27:55 +0200

    Cache encoded names on first encode/decode

--- WhiteTowerGames.DataFixerSharper/Codecs/RecordCodec/FieldCodecs.cs
+++ WhiteTowerGames.DataFixerSharper/Codecs/RecordCodec/FieldCodecs.cs
@@ -29,6 +29,7 @@ public class FieldCodec<T, TField> : IFieldCodec<T, TField>
     private readonly Codec<TField> _codec;
     private readonly string _name;
     private readonly Func<T, TField> _getter;
+    private Dictionary<Type, object> _cachedFieldNames = new(); // we cache the representation of the field name per format to avoid having to encode it every time
 
     public FieldCodec(Codec<TField> codec, string name, Func<T, TField> getter)
     {
@@ -39,28 +40,37 @@ public class FieldCodec<T, TField> : IFieldCodec<T, TField>
 
     public DataResult<(TField, TFormat)> Decode<TFormat>(IDynamicOps<TFormat> ops, TFormat input)
     {
+        var key = _cachedFieldNames.TryGetValue(typeof(TFormat), out var formatted)
+            ? (TFormat)formatted
+            : CacheName(ops);
+
         var fieldResult = ops.GetValue(input, _name);
         if (fieldResult.IsError)
-        {
-            Console.WriteLine("field not found");
-            Console.WriteLine(fieldResult.ErrorMessage);
             return DataResult<(TField, TFormat)>.Fail(fieldResult.ErrorMessage);
-        }
 
         var parsed = _codec.Parse(ops, fieldResult.GetOrThrow());
         if (parsed.IsError)
             return DataResult<(TField, TFormat)>.Fail(parsed.ErrorMessage);
 
-        return parsed.Map(decoded =>
-            (decoded, ops.RemoveFromInput(input, ops.CreateString(_name)))
-        );
+        return parsed.Map(decoded => (decoded, ops.RemoveFromInput(input, key)));
     }
 
     public DataResult<TFormat> Encode<TFormat>(T input, IDynamicOps<TFormat> ops, TFormat prefix)
     {
+        var key = _cachedFieldNames.TryGetValue(typeof(TFormat), out var formatted)
+            ? (TFormat)formatted
+            : CacheName(ops);
+
         var fieldValue = _getter(input);
         var encoded = _codec.EncodeStart(ops, fieldValue);
-        return encoded.Map(e => ops.MergeAndAppend(prefix, ops.CreateString(_name), e));
+        return builder.Add(key, encoded);
+    }
+
+    private TFormat CacheName<TFormat>(IDynamicOps<TFormat> ops)
+    {
+        var value = ops.CreateString(_name)!;
+        _cachedFieldNames[typeof(TFormat)] = value;
+        return value;
     }
 }
 
@@ -70,6 +80,7 @@ public class OptionalFieldCodec<T, TField> : IFieldCodec<T, TField>
     private readonly string _name;
     private readonly Func<T, TField> _getter;
     private readonly TField _defaultValue;
+    private Dictionary<Type, object> _cachedFieldNames = new();
 
     public OptionalFieldCodec(
         Codec<TField> codec,
@@ -86,35 +97,38 @@ public class OptionalFieldCodec<T, TField> : IFieldCodec<T, TField>
 
     public DataResult<(TField, TFormat)> Decode<TFormat>(IDynamicOps<TFormat> ops, TFormat input)
     {
+        var key = _cachedFieldNames.TryGetValue(typeof(TFormat), out var formatted)
+            ? (TFormat)formatted
+            : CacheName(ops);
+
         var fieldResult = ops.GetValue(input, _name);
         if (fieldResult.IsError)
-        {
-            Console.WriteLine("field not found");
-            Console.WriteLine(fieldResult.ErrorMessage);
             return DataResult<(TField, TFormat)>.Success((_defaultValue, input));
-        }
 
         var parsed = _codec.Parse(ops, fieldResult.GetOrThrow());
         if (parsed.IsError)
             return DataResult<(TField, TFormat)>.Fail(parsed.ErrorMessage);
 
-        return parsed.Map(decoded =>
-            (decoded, ops.RemoveFromInput(input, ops.CreateString(_name)))
-        );
+        return parsed.Map(decoded => (decoded, ops.RemoveFromInput(input, key)));
     }
 
     public DataResult<TFormat> Encode<TFormat>(T input, IDynamicOps<TFormat> ops, TFormat prefix)
     {
+        var key = _cachedFieldNames.TryGetValue(typeof(TFormat), out var formatted)
+            ? (TFormat)formatted
+            : CacheName(ops);
+
         var fieldValue = _getter(input);
 
-        var key = ops.CreateString(_name);
         var valueResult = _codec.EncodeStart(ops, fieldValue);
 
-        if (valueResult.IsError)
-            return valueResult;
+        return builder.Add(key, valueResult);
+    }
 
-        var appended = ops.MergeAndAppend(prefix, key, valueResult.GetOrThrow());
-
-        return DataResult<TFormat>.Success(appended);
+    private TFormat CacheName<TFormat>(IDynamicOps<TFormat> ops)
+    {
+        var value = ops.CreateString(_name)!;
+        _cachedFieldNames[typeof(TFormat)] = value;
+        return value;
     }
 }
