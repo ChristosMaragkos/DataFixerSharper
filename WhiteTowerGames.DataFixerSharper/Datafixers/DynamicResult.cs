4669db35a21b1c6394471ace85b75907dbc5289d
Christos Maragkos <whitetowersoftware@gmail.com>
2026-02-18 23:24:53 +0200

    Cleanup

--- WhiteTowerGames.DataFixerSharper/Datafixers/DynamicResult.cs
+++ WhiteTowerGames.DataFixerSharper/Datafixers/DynamicResult.cs
@@ -46,14 +46,14 @@ public readonly struct DynamicResult<TFormat>
         return Data.Set(key, valueResult.Data);
     }
 
+    public DynamicResult<TFormat> Map(Func<Dynamic<TFormat>, DynamicResult<TFormat>> mapper) =>
+        _result.Map(mapper).GetOrElse(this);
+
     public Dynamic<TFormat> GetOrElse(Dynamic<TFormat> defaultValue) =>
         !IsError ? Data : defaultValue;
 
     public Dynamic<TFormat> GetOrThrow() =>
         !IsError ? Data : throw new InvalidOperationException(ErrorMessage);
-
-    public DynamicResult<TFormat> Map(Func<Dynamic<TFormat>, Dynamic<TFormat>> mapper) =>
-        _result.Map<Dynamic<TFormat>>(mapper);
 
     public static implicit operator DynamicResult<TFormat>(DataResult<Dynamic<TFormat>> result) =>
         new DynamicResult<TFormat>(result);
@@ -63,9 +63,6 @@ public readonly struct DynamicResult<TFormat>
     public bool IsError => _result.IsError;
     public string ErrorMessage => _result.ErrorMessage;
     private Dynamic<TFormat> Data => _result.GetOrThrow();
-
-    private static DynamicResult<TFormat> Fail(string errorMessage) =>
-        new DynamicResult<TFormat>(DataResult<Dynamic<TFormat>>.Fail(errorMessage));
 
     private static DynamicResult<TFormat> Success(Dynamic<TFormat> result) =>
         new DynamicResult<TFormat>(DataResult<Dynamic<TFormat>>.Success(result));
