975c26bfce5d74d225b58137dc8558d0d0d91bee
Christos Maragkos <whitetowersoftware@gmail.com>
2026-02-18 23:01:36 +0200

    Add DataFixer registry

---
+++ WhiteTowerGames.DataFixerSharper/Datafixers/DataFixer.cs
@@ -0,0 +1,40 @@
+using WhiteTowerGames.DataFixerSharper.Abstractions;
+
+namespace WhiteTowerGames.DataFixerSharper.Datafixers;
+
+public static class DataFixer
+{
+    public static readonly SortedDictionary<Version, List<IDataFix>> DataFixes = new();
+
+    public static DataResult<TFormat> Migrate<TOps, TFormat>(
+        Version fromVersion,
+        Version toVersion,
+        TOps ops,
+        TFormat data
+    )
+        where TOps : IDynamicOps<TFormat>
+    {
+        var migrating = new Dynamic<TFormat>(ops, data);
+        foreach (var (version, fixes) in DataFixes)
+        {
+            if (version < fromVersion)
+                continue;
+
+            if (version >= toVersion)
+                break;
+
+            foreach (var fix in fixes)
+            {
+                var applied = fix.Apply(migrating);
+                if (applied.IsError)
+                    return DataResult<TFormat>.Fail(
+                        $"Could not migrate data to version {version}: {applied.ErrorMessage}"
+                    );
+
+                migrating = applied.GetOrThrow();
+            }
+        }
+
+        return DataResult<TFormat>.Success(migrating.Value);
+    }
+}
