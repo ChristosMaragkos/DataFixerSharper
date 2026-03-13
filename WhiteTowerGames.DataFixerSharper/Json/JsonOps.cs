ed0633e5d0cc105fb29b0b89685ff7e64a41cf3d
Christos Maragkos <whitetowersoftware@gmail.com>
2026-03-13 18:25:38 +0200

    Add TODO

--- WhiteTowerGames.DataFixerSharper/Json/JsonOps.cs
+++ WhiteTowerGames.DataFixerSharper/Json/JsonOps.cs
@@ -6,6 +6,7 @@ using WhiteTowerGames.DataFixerSharper.Abstractions;
 
 namespace WhiteTowerGames.DataFixerSharper.Json;
 
+// TODO: Implement batch map/list ops. Streaming collections once per new item is a crime against software engineering.
 public sealed class JsonOps : IDynamicOps<JsonByteBuffer>
 {
     public static JsonOps Instance { get; } = new();
@@ -51,6 +52,7 @@ public sealed class JsonOps : IDynamicOps<JsonByteBuffer>
         return new JsonByteBuffer(buffer);
     }
 
+    // TODO: Stackalloc for strings less than ~128 bytes since this monstrosity always allocates a buffer on the heap under the hood
     public JsonByteBuffer CreateString(string value) => JsonSerializer.SerializeToUtf8Bytes(value);
 
     public JsonByteBuffer CreateBool(bool value) => value ? TrueValue : FalseValue;
